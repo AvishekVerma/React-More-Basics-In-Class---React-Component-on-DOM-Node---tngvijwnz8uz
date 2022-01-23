55048836ab79f1c3d6c893b1bb1578e28c80bc8e
Avishek <avishekverma298@gmail.com>
2022-01-23 18:53:55 +0530

    p tag added in JSX

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-{/* <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p> */}
+
 
 import React from "react";
 import "./../styles/App.css";
@@ -6,7 +6,7 @@ import "./../styles/App.css";
 function App () {
     return (
         <div id="root">
-
+            <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p> 
         </div>
     );
 }
